b8da0a137cb9c4354457f873080ba6ef8be8c656
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-11 03:16:32 +0000

    Updated server.js"
    ""
    "Update server.js"
    
    "
    ""

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log('Server running on port ${PORT}'));
-})
+const startServer = async () => {
+    await connectDB(); //connect first
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+};
+
