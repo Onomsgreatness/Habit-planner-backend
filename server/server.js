7e7c7369064f0921e1ffdfb692045d9e6d9385d8
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-11 03:54:44 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,25 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/dbConn");
 
-//Load environment variables
-dotenv.config({path: "./config.env"});
+// Load env variables
+dotenv.config({ path: ".env" });
 
 const app = express();
-app.use(express.json());    //Middleware
+app.use(express.json());
 
-//Connect DB
-connectDB();
-
-//Simple test route
+// Test route
 app.get("/", (req, res) => {
-    res.send("API is running");
+  res.send("API is running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-    await connectDB(); //connect first
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  await connectDB(); // connect DB first
+  app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
 };
 
 startServer();
