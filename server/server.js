3f6d58c6f7517ab592f63c4e0c4291ef64141c52
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-11 02:52:09 +0000

    Added Server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = requires("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/dbConn");
+
+dotenv.config({path: "./config.env"});
+
+const app = express();
+app.use(express.json());
+
+//Connect DB
+connectDB();
+
+//Simple test route
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+})
