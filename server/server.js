ed369a1d91b019fba31612f3372a179200e4121c
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-13 13:42:16 +0000

    Update server.js as added swagger.yaml

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/dbConn");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
 // Load env variables
 dotenv.config({ path: ".env" });
 
 const app = express();
 app.use(express.json());
+
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // Test route
 app.get("/", (req, res) => {
@@ -29,3 +33,4 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/test", require("./routes/testRoutes"));
 app.use("/api/habits", require("./routes/habitRoutes"));
 app.use("/api/progress", require("./routes/progressRoutes"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
