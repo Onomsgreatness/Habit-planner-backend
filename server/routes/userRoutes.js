7acca159a85b2963af1b0f11b4c31a4a92000fbd
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-20 17:33:47 +0000

    updated endpoints for me and updateMe

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/userController");
+const auth = require("../middleware/auth");
+const { register, login, me, updateMe } = require("../controllers/userController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me", auth, me);
+router.put("/updateMe", auth, updateMe);
 
 module.exports = router;
