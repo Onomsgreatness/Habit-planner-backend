f20f45285709f7a9a2db60f15745a170554a8005
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-20 18:47:32 +0000

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,6 @@ const { register, login, me, updateMe } = require("../controllers/userController
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", auth, me);
-router.put("/me", auth, updateMe);
+router.put("/updateMe", auth, updateMe);
 
 module.exports = router;
