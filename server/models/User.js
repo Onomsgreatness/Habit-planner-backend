09ba25d3f5dc3f0a967f731796a98cc26b9a4d09
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2026-01-20 17:04:57 +0000

    Modified user.js to have a field/parameter, name

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    name: { type: String, trim: true, maxlength: 60 },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     passwordHash: { type: String, required: true },
   },
